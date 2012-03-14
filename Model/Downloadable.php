4eddb64fb0ad640905b36396cdd292ad1cae3b82
Luis Cordova <cordoval@gmail.com>
2012-03-14 00:37:57 -0500

    added handler, downloadable model, and di config for downloadablebundle

---
+++ Model/Downloadable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\ProductDownloadableBundle\Model;
+
+use Vespolina\ProductBundle\Model\Product;
+
+class Downloadable extends Product
+{
+
+}
